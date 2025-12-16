6bcd7467eb57fa76086f9e0df2e089924435a674
BayonetArch <sudipxd124@gmail.com>
2025-12-16 19:34:24 +0545

    Refactor: Name prefix added to resolve name collisions UPDATE  : Updated README with more usage instructions

--- README.md
+++ README.md
@@ -1,15 +1,92 @@
 # Introduction #
 
-C header for some simple functions.
+C header for some essential macros and functions.
 
 # Quick Start #
 
 ```bash
-    wget https://raw.githubusercontent.com/BayonetArch/essen.h/refs/heads/master/essen.h
+ wget https://raw.githubusercontent.com/BayonetArch/essen.h/refs/heads/master/essen.h
 ```
 
 in a c file: 
 
 ```c
-    #include "essen.h"
+ #include "essen.h"
 ```
+
+
+# Useful Macros and Functions #
+
+1. Dynamic Array
+
+header contains dynamic array which we can append to and iterate over.firstly define `ESL_DA` before include
+
+```c
+#define ESL_DA
+#include "essen.h"
+```
+
+DynamicArray is an Struct like this :
+
+```c
+typedef struct {
+    int   *items;
+    size_t count;
+    size_t capacity;
+} Da;
+
+```
+
+we can append to it
+
+```c
+    Da xs = {0};
+
+    esl_da_append(&xs, 20);
+    esl_da_append(&xs, 40);
+    esl_da_append(&xs, 60);
+    
+    esl_da_free(&xs);
+
+```
+
+We can also iterate over dynamic array 
+
+```c
+    esl_da_foreach(int, it, &xs)
+    {
+        size_t indx = it - xs.items; // 'it' is the iterator starting from pointer to first element
+        esl_println("%ld:%d", indx, *it);
+    }
+
+```
+
+2. StringBuilder
+
+Similar to Dynamic Arrays it has StringBuilder.
+
+Full example :
+
+```c
+
+#define ESL_SB
+#include "essen.h"
+
+int main(void)
+{
+    EslStringBuilder sb = {0};
+    esl_sb_init(&sb);
+    esl_sb_append(&sb, "Foo");
+    esl_sb_append(&sb, "Bar");
+
+    esl_println("%s", esl_sb_to_string(&sb));
+    esl_sb_free(&sb);
+    return EXIT_SUCCESS;
+}
+
+```
+
+
+> [!TIP]
+> you can define `ESL_SHORT_NAMES` to get rid of esl prefix.
+> for eg: esl_da_append -> da_append
--- essen.h
+++ essen.h
@@ -6,25 +6,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef enum : int { LOG_INFO = 0, LOG_WARN = 1, LOG_ERROR = 2 } LogLevel;
+#ifdef ESL_SHORT_NAMES
+
+#define LogLevel Esl_LogLevel;
+#define LOG_INFO ESL_LOG_INFO
+#define LOG_WARN ESL_LOG_WARN
+#define LOG_ERROR ESL_LOG_ERROR
+#define UNUSED ESL_UNUSED
+#define LAST_ERROR ESL_LAST_ERROR
+#define SWAP ESL_SWAP)
+#define current_date esl_current_date
+#define eprintln esl_eprintln
+#define println esl_println
+#define fatalf esl_fatalf
+#define flog esl_flog
+#define fflog esl_fflog
+#define shift_args esl_shift_args
+
+#endif // ESL_SHORT_NAMES
+
+typedef enum : int {
+    ESL_LOG_INFO  = 0,
+    ESL_LOG_WARN  = 1,
+    ESL_LOG_ERROR = 2
+} Esl_LogLevel;
 
 #define ARR_LEN(arr) (sizeof(arr) / sizeof(arr[0]))
-#define LAST_ERROR() strerror(errno)
+#define ESL_LAST_ERROR() strerror(errno)
 #define FILE_LOC() __FILE__, __LINE__
 #define SEPARATOR() println("───────────────────────────────────────")
 
-#define SWAP(T, a, b)                                                          \
+#define ESL_SWAP(T, a, b)                                                      \
     do {                                                                       \
         T t = a;                                                               \
         a   = b;                                                               \
         b   = t;                                                               \
     } while (0)
 
-#define UNUSED(x) (void)x
+#define ESL_UNUSED(x) (void)x
 
 #include <time.h>
 
-static inline char *current_date(char *time_buf, size_t buf_size)
+__attribute__((unused)) static inline char *esl_current_date(char  *time_buf,
+                                                             size_t buf_size)
 {
     time_t    t    = time(NULL);
     struct tm date = *localtime(&t);
@@ -46,10 +70,10 @@ static inline char *current_date(char *time_buf, size_t buf_size)
 #define ANSI_YELLOW_BOLD(x)         "\x1b[1;33m" x ANSI_RESET
 // clang-format on
 
-#define println(fmt, ...) printf(fmt "\n", ##__VA_ARGS__)
-#define eprintln(fmt, ...) fprintf(stderr, fmt "\n", ##__VA_ARGS__)
+#define esl_println(fmt, ...) printf(fmt "\n", ##__VA_ARGS__)
+#define esl_eprintln(fmt, ...) fprintf(stderr, fmt "\n", ##__VA_ARGS__)
 
-#define fatalf(exit_code, fmt, ...)                                            \
+#define esl_fatalf(exit_code, fmt, ...)                                        \
     do {                                                                       \
         fprintf(stderr,                                                        \
                 ANSI_GREY("%s:%d") " " ANSI_RED("Fatal") ": " fmt "\n",        \
@@ -58,12 +82,12 @@ static inline char *current_date(char *time_buf, size_t buf_size)
     } while (0)
 
 // file logging
-#define flog(fp, log_level, color, fmt, ...)                                   \
+#define esl_flog(fp, log_level, color, fmt, ...)                               \
     do {                                                                       \
         char time_buf[64];                                                     \
         current_date(time_buf, sizeof(time_buf));                              \
         switch ((log_level)) {                                                 \
-        case LOG_WARN:                                                         \
+        case ESL_LOG_WARN:                                                     \
             if (color) {                                                       \
                 fprintf(fp,                                                    \
                         "%s "                                                  \
@@ -77,7 +101,7 @@ static inline char *current_date(char *time_buf, size_t buf_size)
                         time_buf, ##__VA_ARGS__);                              \
             }                                                                  \
             break;                                                             \
-        case LOG_INFO:                                                         \
+        case ESL_LOG_INFO:                                                     \
             if (color) {                                                       \
                 fprintf(fp,                                                    \
                         "%s "                                                  \
@@ -91,7 +115,7 @@ static inline char *current_date(char *time_buf, size_t buf_size)
                         time_buf, ##__VA_ARGS__);                              \
             }                                                                  \
             break;                                                             \
-        case LOG_ERROR:                                                        \
+        case ESL_LOG_ERROR:                                                    \
             if (color) {                                                       \
                 fprintf(fp,                                                    \
                         "%s "                                                  \
@@ -106,17 +130,18 @@ static inline char *current_date(char *time_buf, size_t buf_size)
             }                                                                  \
             break;                                                             \
         default:                                                               \
-            fatalf(1, "Not a valid log level");                                \
+            esl_fatalf(1, "Not a valid log level");                            \
         };                                                                     \
     } while (0)
 
-#define fflog(fp1, fp2, log_level, color_for_f1, color_for_f2, fmt, ...)       \
+#define esl_fflog(fp1, fp2, log_level, color_for_f1, color_for_f2, fmt, ...)   \
     do {                                                                       \
-        flog(fp1, log_level, color_for_f1, fmt, ##__VA_ARGS__);                \
-        flog(fp2, log_level, color_for_f2, fmt, ##__VA_ARGS__);                \
+        esl_flog(fp1, log_level, color_for_f1, fmt, ##__VA_ARGS__);            \
+        esl_flog(fp2, log_level, color_for_f2, fmt, ##__VA_ARGS__);            \
     } while (0)
 
-static inline const char *shift_args(int *argc, char **argv)
+__attribute__((unused)) static inline const char *esl_shift_args(int   *argc,
+                                                                 char **argv)
 {
     if (*argc == 1)
         return NULL;
@@ -129,54 +154,63 @@ static inline const char *shift_args(int *argc, char **argv)
     (*argc)--;
     return program;
 }
+#ifdef ESL_DA
 
-#ifdef DA
+#define ESL_DA_DEF_CAP 256
 
-#define DA_DEF_CAP 256
-
-#define da_append(xs, x)                                                       \
+#define esl_da_append(xs, x)                                                   \
     do {                                                                       \
         if ((xs)->capacity == 0) {                                             \
-            (xs)->capacity = DA_DEF_CAP;                                       \
-            (xs)->item     = malloc((xs)->capacity * sizeof(*(xs)->item));     \
-            if (!(xs)->item)                                                   \
-                fatalf(1, "memory allocation failed");                         \
+            (xs)->capacity = ESL_DA_DEF_CAP;                                   \
+            (xs)->items    = malloc((xs)->capacity * sizeof(*(xs)->items));    \
+            if (!(xs)->items)                                                  \
+                esl_fatalf(1, "memory allocation failed");                     \
         }                                                                      \
         else if ((xs)->count >= (xs)->capacity) {                              \
             (xs)->capacity *= 2;                                               \
             void *tmp =                                                        \
-                realloc((xs)->item, (xs)->capacity * sizeof(*(xs)->item));     \
+                realloc((xs)->items, (xs)->capacity * sizeof(*(xs)->items));   \
             if (!tmp)                                                          \
-                fatalf(1, "memory reallocation failed");                       \
-            (xs)->item = tmp;                                                  \
+                esl_fatalf(1, "memory reallocation failed");                   \
+            (xs)->items = tmp;                                                 \
         }                                                                      \
-        (xs)->item[(xs)->count++] = x;                                         \
+        (xs)->items[(xs)->count++] = x;                                        \
     } while (0)
 
-#define da_free(xs)                                                            \
+#define esl_da_free(xs)                                                        \
     do {                                                                       \
-        free((xs)->item);                                                      \
-        ((xs))->item     = NULL;                                               \
+        free((xs)->items);                                                     \
+        ((xs))->items    = NULL;                                               \
         ((xs))->capacity = 0;                                                  \
         ((xs))->count    = 0;                                                  \
     } while (0)
 
-#define da_foreach(Type, it, da)                                               \
-    for (Type *it = (da)->item; it < (da)->item + (da)->count; ++it)
+#define esl_da_foreach(Type, it, da)                                           \
+    for (Type *it = (da)->items; it < (da)->items + (da)->count; ++it)
 
-#define da_free_heap(type, xs)                                                 \
+#define esl_da_free_heap(type, xs)                                             \
     do {                                                                       \
         da_foreach(type, it, xs) { free(*it); }                                \
         da_free((xs));                                                         \
-        (xs)->item     = NULL;                                                 \
+        (xs)->items    = NULL;                                                 \
         (xs)->capacity = 0;                                                    \
         (xs)->count    = 0;                                                    \
     } while (0);
 
-#endif // DA
+#ifdef ESL_SHORT_NAMES
 
-#ifdef SB
-#define DEF_SB_CAP 64
+#define da_append(xs, x) esl_da_append(xs, x)
+#define da_free esl_da_free
+#define da_foreach(Type, it, da) esl_da_foreach(Type, it, da)
+#define da_free_heap(type, xs) esl_da_free_heap(type, xs)
+
+#endif /* ifdef ESL_SHORT_NAMES */
+
+#endif // ESL_DA
+
+#ifdef ESL_SB
+
+#define ESL_DEF_SB_CAP 64
 #include <stddef.h>
 
 typedef struct {
@@ -184,19 +218,19 @@ typedef struct {
     size_t size;
     size_t capacity;
 
-} StringBuilder;
+} EslStringBuilder;
 
-static inline void sb_init(StringBuilder *sb)
+static inline void esl_sb_init(EslStringBuilder *sb)
 {
-    sb->capacity = DEF_SB_CAP;
+    sb->capacity = ESL_DEF_SB_CAP;
     sb->buf      = malloc(sb->capacity);
     if (!sb->buf) {
-        fatalf(1, "memory allocation failed");
+        esl_fatalf(1, "memory allocation failed");
     }
     sb->size = 0;
 }
 
-static inline void sb_append(StringBuilder *sb, char *str)
+static inline void esl_sb_append(EslStringBuilder *sb, char *str)
 {
     size_t n = strlen(str);
 
@@ -205,21 +239,21 @@ static inline void sb_append(StringBuilder *sb, char *str)
     }
     char *tmp = realloc(sb->buf, sb->capacity);
     if (!tmp) {
-        fatalf(1, "Reallocation failed");
+        esl_fatalf(1, "Reallocation failed");
     }
     sb->buf = tmp;
     memcpy(sb->buf + sb->size, str, n);
     sb->size += n;
 }
 
-static inline char *sb_to_string(StringBuilder *sb)
+static inline char *esl_sb_to_string(EslStringBuilder *sb)
 {
     sb->buf[sb->size] = '\0';
 
     return sb->buf;
 }
 
-static inline void sb_free(StringBuilder *sb)
+static inline void esl_sb_free(EslStringBuilder *sb)
 {
     free(sb->buf);
     sb->buf      = NULL;
@@ -227,5 +261,15 @@ static inline void sb_free(StringBuilder *sb)
     sb->size     = 0;
 }
 
-#endif /* ifdef SB */
+#ifdef ESL_SHORT_NAMES
+
+#define sb_init esl_sb_init
+#define sb_append esl_sb_append
+#define sb_to_string esl_sb_to_string
+#define sb_free esl_sb_free
+typedef EslStringBuilder StringBuilder;
+
+#endif /* ifdef ESL_SHORT_NAMES */
+
+#endif /* ifdef ESL_SB */
 #endif // ESSENTIAL_H
